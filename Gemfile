8fe241cfa66d617967115f2b91178ddca240d1a0
Nathan Rofkahr <nathan.rofkahr@gmail.com>
2015-04-11 22:41:40 -0500

    Generated a user model

--- Gemfile
+++ Gemfile
@@ -12,7 +12,9 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 
+gem 'devise', '~> 3.4.1'
 gem 'bootstrap-sass'
+
 
 group :development, :test do
 	gem 'sqlite3'	
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # Required for Heroku
+  #Note to set this to your actual host - change to...totally-pinteresting.com
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
