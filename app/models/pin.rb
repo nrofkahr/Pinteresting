627b19f4e97546acff6d7173f6fe72f37b824790
Nathan Rofkahr <nathan.rofkahr@gmail.com>
2015-04-12 22:20:31 -0500

    rolled back experimental pdf data

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,8 +4,4 @@ class Pin < ActiveRecord::Base
 	
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-  
-	# has_attached_file :document, styles: { thumb: { geometry: "150x150", format: "jpg" } }, processors: [:ghostscript]
-  # validates_attachment_content_type :document, :content_type => ['application/pdf', 'application/msword', 'text/plain']
-  
 end
